281a38af7d373b30226b2f00f13edfa7c2741bba
Flanty <119012794+Bradlu378@users.noreply.github.com>
2025-04-14 17:43:34 +0300

    refactoring

--- src/main/java/LettuceRedisStreamExample.java
+++ src/main/java/LettuceRedisStreamExample.java
@@ -5,9 +5,7 @@ import java.util.Map;
 
 public class LettuceRedisStreamExample {
     public static void main(String[] args) throws InterruptedException {
-        // Подключение к Redis
         RedisClient client = RedisClient.create("redis://127.0.0.1:6500");
-        //RedisClient client = RedisClient.create("redis://senko.netcraze.club:6500");
         StatefulRedisConnection<String, String> connection = client.connect();
         RedisCommands<String, String> commands = connection.sync();
 
@@ -23,26 +21,7 @@ public class LettuceRedisStreamExample {
             fields.put("da", "lo" + i);
             fields.put("net", "do" + i);
             commands.xadd(streamName, fields);
-            //Thread.sleep(500);
         }
-
-        //List<StreamMessage<String, String>> messages = commands.xread(
-        //        XReadArgs.Builder.block(0).count(1),
-        //        XReadArgs.StreamOffset.from(streamName, "0-0")
-        //);
-//
-        //if (!messages.isEmpty()) {
-        //    StreamMessage<String, String> firstMessage = messages.get(0);
-        //    System.out.println("\nПервая запись в потоке:");
-        //    System.out.println("ID: " + firstMessage.getId());
-        //    System.out.println("Данные:");
-        //    firstMessage.getBody().forEach((key, value) ->
-        //            System.out.println("  " + key + ": " + value)
-        //    );
-        //} else {
-        //    System.out.println("Поток пуст");
-        //}
-//
         connection.close();
         client.shutdown();
     }
